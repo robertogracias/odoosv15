c1457555fd61701c34d840386ebdcdd6c91d76e3
Roberto Gracias <roberto.gracias@strategi-k.com>
2019-07-11 05:13:28 -0600

    Sprint 1

--- fiaes/__manifest__.py
+++ fiaes/__manifest__.py
@@ -13,11 +13,13 @@
     "author": "Strategi-k",
     "website": "http://strategi-k.com",
     "version": '12.0.0.4',
-    "depends": ['stock','purchase','hr'],
+    "depends": ['stock','purchase','hr','fleet','sale_management'],
     "data": [
         'security/ir.model.access.csv'
+        ,'views/fiaes.xml'
         ,'views/catalogos.xml'
         ,'views/catalogos_d.xml'
+
     ],
     "installable": True,
     "application": True,
