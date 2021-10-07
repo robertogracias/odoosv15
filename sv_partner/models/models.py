4569e5706ad6f6a40ae8c267cad881a522708728
leonelgracias <leonelgracias@gmail.com>
2021-10-07 15:26:20 -0600

    referencias persoanles

--- sv_partner/models/models.py
+++ sv_partner/models/models.py
@@ -18,15 +18,15 @@ _logger = logging.getLogger(__name__)
 
 
     
-class integrador_category(models.Model):
+class odoosv_category(models.Model):
     _inherit='product.category'
     code=fields.Integer("Codigo",select=True)
 
-class integrador_user(models.Model):
+class odoosv_user(models.Model):
     _inherit='res.users'
     code=fields.Integer("Codigo",select=True)
 
-class integrador_partner(models.Model):
+class odoosv_partner(models.Model):
     _inherit='res.partner'
     _sql_constraints = [
         ('NIT_Unico', 'unique (company_id,nit)', 'NIT debe ser unico por Compania'),
@@ -40,4 +40,19 @@ class integrador_partner(models.Model):
     nombres=fields.Char("Nombres")
     apellidos=fields.Char("Apellidos")
     razon_social=fields.Char("Razón social")
+    referencia_ids=fields.One2many(comodel_name='odoosv.partner.referencia',inverse_name='partner_id',string='Referencias')
+
+class odoosv_move(models.Model):
+    _inherit='account.move'
+    nit=fields.Char("NIT",related='partner_id.nit')
+
+class odoosv_referencia(models.Model):
+    _name='odoosv.partner.referencia'
+    _description='Referencia crediticia'
+
+    name=fields.Char("Nombre")
+    telefono=fields.Char("Telefono")
+    email=fields.Char("Email")
+    comentario=fields.Char("Comentario")
+    partner_id=fields.Many2one('res.partner',string="Contacto")
 
