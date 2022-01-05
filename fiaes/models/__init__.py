f3b84342ebf56a5efbb730f3e696a69098ac2139
leonelgracias <leonelgracias@gmail.com>
2022-01-05 06:31:19 -0600

    integrador de sap

--- fiaes/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import catalogos
-from . import activo
-from . import flota
-from . import proyecto
--- fiaes/models/catalogos_d.py
+++
@@ -1,5 +0,0 @@
-from odoo import api, models, fields, _
-from datetime import datetime, timedelta
-from odoo.exceptions import UserError, ValidationError
-from odoo.tools import float_is_zero, float_compare, DEFAULT_SERVER_DATETIME_FORMAT
-from odoo import SUPERUSER_ID
